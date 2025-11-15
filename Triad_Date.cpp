8c15fa91f96e5d9391d72adafec28ef417e997d6
Олександр <88493901+juskurd@users.noreply.github.com>
2025-11-15 04:21:30 +0200

    Add files via upload

---
+++ Triad_Date.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+class Triad {
+protected:
+    int a, b, c;
+
+public:
+    Triad(int x = 0, int y = 0, int z = 0)
+        : a(x), b(y), c(z) {
+    }
+
+    virtual void increase() {
+        a++;
+        b++;
+        c++;
+    }
+
+    virtual void display() const {
+        cout << "Трійка: (" << a << ", " << b << ", " << c << ")";
+    }
+
+    virtual ~Triad() {}
+};
+
+bool isLeap(int y) {
+    return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+}
+
+int daysInMonth(int m, int y) {
+    if (m == 2) return isLeap(y) ? 29 : 28;
+    if (m == 4 || m == 6 || m == 9 || m == 11) return 30;
+    return 31;
+}
+
+class Date : public Triad {
+public:
+    Date(int d = 1, int m = 1, int y = 2000)
+        : Triad(d, m, y) {
+    }
+
+    void addDays(int n) {
+        a += n;
+
+        while (a > daysInMonth(b, c)) {
+            a -= daysInMonth(b, c);
+            b++;
+            if (b > 12) {
+                b = 1;
+                c++;
+            }
+        }
+    }
+
+    void increase() override {
+        addDays(1);
+    }
+
+    void display() const override {
+        cout << "Дата: ";
+        if (a < 10) cout << "0";
+        cout << a << ".";
+        if (b < 10) cout << "0";
+        cout << b << "." << c;
+    }
+};
+
+Triad& processTriad(Triad& t) {
+    t.increase();
+    t.display();
+    cout << endl;
+    return t;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    Triad t(1, 2, 3);
+    Date d(12, 11, 2025);
+
+    cout << "Початкова трійка: ";
+    t.display();
+    cout << endl;
+
+    cout << "Після increase(): ";
+    t.increase();
+    t.display();
+    cout << endl << endl;
+
+    cout << "Початкова дата: ";
+    d.display();
+    cout << endl;
+
+    cout << "Після increase(): ";
+    d.increase();
+    d.display();
+    cout << endl;
+
+    cout << "Після addDays(365): ";
+    d.addDays(365);
+    d.display();
+    cout << endl << endl;
+
+    cout << "Демонстрація принципу підстановки:" << endl;
+    processTriad(t);
+    processTriad(d);
+
+    return 0;
+}
